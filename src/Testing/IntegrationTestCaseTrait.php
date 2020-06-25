a04cb40c75e629e6f2f23fdd228112f83d811ff0
TomasVotruba <tomas.vot@gmail.com>
2020-06-25 16:10:20 +0200

    update README

---
+++ src/Testing/IntegrationTestCaseTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\EasyCodingStandardTester\Testing;
+
+use Nette\Utils\FileSystem;
+use Nette\Utils\Strings;
+use Symplify\EasyTesting\Fixture\FixtureSplitter;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+/**
+ * @inspiration https://github.com/rectorphp/rector/blob/7039ce87c666d787c1d744343d449170d1655355/src/Testing/PHPUnit/IntegrationRectorTestCaseTrait.php
+ */
+trait IntegrationTestCaseTrait
+{
+    /**
+     * @return string[]
+     */
+    protected function splitContentToOriginalFileAndExpectedFile(SmartFileInfo $smartFileInfo): array
+    {
+        $fixtureSplitter = new FixtureSplitter();
+        [$originalContent, $expectedContent] = $fixtureSplitter->splitFileInfoToBeforeAfter($smartFileInfo);
+
+        $originalFile = $this->createTemporaryPathWithPrefix($smartFileInfo, 'original');
+        $expectedFile = $this->createTemporaryPathWithPrefix($smartFileInfo, 'expected');
+        FileSystem::write($originalFile, $originalContent);
+        FileSystem::write($expectedFile, $expectedContent);
+
+        return [$originalFile, $expectedFile];
+    }
+
+    private function createTemporaryPathWithPrefix(SmartFileInfo $smartFileInfo, string $prefix): string
+    {
+        $hash = Strings::substring(md5($smartFileInfo->getPathname()), 0, 5);
+        return sprintf(
+            sys_get_temp_dir() . '/ecs_temp_tests/%s_%s_%s',
+            $prefix,
+            $hash,
+            $smartFileInfo->getBasename('.inc')
+        );
+    }
+}
